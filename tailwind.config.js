564b3b4b9d2283e629b826ba120d349ec8e55132
hfy <hanfangyu2016@126.com>
2026-04-17 19:33:29 +0800

    chore: configure nextjs environment and dependencies for build

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'movie-black': '#050505',
+        'accent-purple': '#6441A5',
+      },
+    },
+  },
+  plugins: [],
+}
